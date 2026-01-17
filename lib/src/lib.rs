73937d3efc2493aa3922a57b083af8157c8482d3
jack <jack@jackcodeland.com>
2026-01-17 23:14:40 +1100

    add unit tests

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -65,3 +65,60 @@ pub fn process_transactions<R: Read, O: Write>(input: R, output: O) -> Result<()
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic_deposit_withdrawal() {
+        let input = "\
+type,       client, tx, amount
+deposit,    1,      1,  10.0
+deposit,    2,      2,  20.0
+withdrawal, 1,      3,  5.0
+withdrawal, 2,      4,  30.0
+";
+        let mut output = Vec::new();
+
+        process_transactions(input.as_bytes(), &mut output).expect("Should not fail");
+
+        let result = String::from_utf8(output).expect("Output should be valid UTF-8");
+
+        assert!(result.contains("1,5,0,5,false"));
+        assert!(result.contains("2,20,0,20,false"));
+    }
+
+    #[test]
+    fn test_dispute_and_chargeback() {
+        let input = "\
+type,       client, tx, amount
+deposit,    1,      1,  100.0
+dispute,    1,      1,
+chargeback, 1,      1,
+";
+        let mut output = Vec::new();
+
+        process_transactions(input.as_bytes(), &mut output).expect("Should not fail");
+
+        let result = String::from_utf8(output).unwrap();
+
+        assert!(result.contains("1,0,0,0,true"));
+    }
+
+    #[test]
+    fn test_security_cross_client_dispute() {
+        let input = "\
+type,       client, tx, amount
+deposit,    1,      1,  100.0
+dispute,    2,      1,
+";
+        let mut output = Vec::new();
+
+        process_transactions(input.as_bytes(), &mut output).unwrap();
+
+        let result = String::from_utf8(output).unwrap();
+
+        assert!(result.contains("1,100,0,100,false"));
+    }
+}
